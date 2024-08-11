6af803af0ead660286c702adc6b7630a9e843a6b
†ZeroCool† <herocool263@gmail.com>
2024-08-12 01:05:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//base by Tech-God
+//base by ❦★𝘿𝘼𝙍𝙆★❦†𝙕𝙀𝙍𝙊𝘾𝙊𝙊𝙇†𓃵
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @techgod143
-//Instagram: techgod143
-//Telegram: t.me/techgod143
-//GitHub: @techgod143
-//WhatsApp: +917466008456
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
+//YouTube: @Jayden01
+//Instagram: ZeroCool263
+//Telegram: t.me/codewhisporer
+//GitHub: @darkzerocool
+//WhatsApp: +263712473551
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@jayden01
 
 const {
    spawn
